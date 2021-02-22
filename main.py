a836af78aefbac5609073c760762d54684a5520c
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-22 17:46:49 +0200

    read names for from invited_names.txt

--- main.py
+++ main.py
@@ -6,3 +6,13 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+names = []
+
+#read names for from invited_names.txt
+with open('input/names/invited_names.txt') as file:
+    file_lines = file.readlines()
+    for line in file_lines:
+        names.append(line)
+
+print(names)
